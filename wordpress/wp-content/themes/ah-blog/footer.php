a55687fff3543b6ff214ec07a59adfa8eb0ff175
Your Name <your.email@example.com>
2025-08-08 03:12:48 +0000

    paraent and child pages

---
+++ wordpress/wp-content/themes/ah-blog/footer.php
@@ -0,0 +1,69 @@
+<footer class="site-footer">
+      <div class="site-footer__inner container container--narrow">
+        <div class="group">
+          <div class="site-footer__col-one">
+            <h1 class="school-logo-text school-logo-text--alt-color">
+              <a href="<?php echo site_url('/index.php/about-me'); ?>"><strong>Adam</strong> Huang</a>
+            </h1>
+            <p><a class="site-footer__link" href="#">adam.huang2022@gmail.com</a></p>
+          </div>
+
+          <div class="site-footer__col-two-three-group">
+            <div class="site-footer__col-two">
+              <h3 class="headline headline--small">Explore</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="<?php echo site_url('/index.php/about-me'); ?>">About Me</a></li>
+                  <li><a href="#">Projects</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <!-- <li><a href="#">Campuses</a></li> -->
+                </ul>
+              </nav>
+            </div>
+
+            <!-- <div class="site-footer__col-three">
+              <h3 class="headline headline--small">Learn</h3>
+              <nav class="nav-list">
+                <ul>
+                  <li><a href="#">Legal</a></li>
+                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">Careers</a></li>
+                </ul>
+              </nav>
+            </div> -->
+          </div>
+
+          <div class="site-footer__col-four">
+            <h3 class="headline headline--small">Connect With Me</h3>
+            <nav>
+              <ul class="min-list social-icons-list group">
+                 <li>
+                  <a href="https://www.linkedin.com/in/adamhuangus/" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                </li>
+                 <li>
+                  <a href="https://x.com/adam_huang_2023" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="https://github.com/adam-huang-us/adam-huang-us/blob/main/README.md" class="social-color-github"><i class="fa fa-github" aria-hidden="true"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="social-color-medium"><i class="fa fa-medium" aria-hidden="true"></i></a>
+                </li>
+               
+                <!-- <li>
+                  <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                </li>
+               
+                <li>
+                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li> -->
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wordpress/wp-content/themes/ah-blog/index.php
@@ -0,0 +1,109 @@
+<?php get_header(); ?>
+
+  <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/screenshot.png') ?>);"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large">Welcome!</h1>
+        <h2 class="headline headline--medium">Let&rsquo;s become a better developer together.</h2>
+        <!-- <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+        <a href="#" class="btn btn--large btn--blue">Find Your Major</a> -->
+      </div>
+    </div>
+
+    <div class="full-width-split group">
+      <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Projects</h2>
+
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Aug</span>
+              <span class="event-summary__day">01</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Personal Blog Website</a></h5>
+              <p>Build personal blog website with LAMP on EC2. <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Sep</span>
+              <span class="event-summary__day">01</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Digital Personal Assistant</a></h5>
+              <p>Build a digital personal assistant using LLM with RAG. <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Projects</a></p>
+        </div>
+      </div>
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Blogs</h2>
+
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+              <span class="event-summary__month">Aug</span>
+              <span class="event-summary__day">01</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Deep Dive into CPU</a></h5>
+              <p>Everything you need to know about CPU as developer. <a href="#" class="nu gray">Read more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+              <span class="event-summary__month">Spe</span>
+              <span class="event-summary__day">01</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Beyond the Dashboard: A 3-Month Journey to Data Engineering.</a></h5>
+              <p>Building my 3-Month Plan to Transition from Data Analyst to Data Engineer. <a href="#" class="nu gray">Read more</a></p>
+            </div>
+          </div>
+
+          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="hero-slider">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Transportation</h2>
+                <p class="t-center">All students have free unlimited bus fare.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+                <p class="t-center">Our dentistry program recommends eating apples.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Food</h2>
+                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+      </div>
+    </div>
+
+  <?php get_footer();
+
+?>
